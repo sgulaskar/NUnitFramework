bb46b60584934fc26029d911817e9b6601238169
sgulaskar <sangram.gulaskar@outlook.com>
2023-04-20 18:06:21 +0530

    Added buzz page

---
+++ NUNIT_Framework/Pages/BuzzPage.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using NUNIT_Framework.UIOperations;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NUNIT_Framework.Pages
+{
+    public class BuzzPage : UIActions
+    {
+        public static IWebElement BuzzButton => driver.FindElement(By.XPath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[11]/a"));
+
+        public static IWebElement TextBox => driver.FindElement(By.XPath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div/div[1]/div[1]/div[2]/form/div/textarea"));
+
+        public static IWebElement PostButton => driver.FindElement(By.XPath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div/div[1]/div[1]/div[2]/form/div/div/button"));
+        public BuzzPage BuzzPost()
+        {
+            BuzzButton.Click();
+            Thread.Sleep(2000);
+            TextBox.SendKeys("Hello Automation");
+            Thread.Sleep(2000);
+            PostButton.Click();
+            Thread.Sleep(2000);
+
+
+
+            return new BuzzPage();
+
+        }
+    }
+}
